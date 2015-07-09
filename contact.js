e103d6bbe8429ac06dfd0e9e00ac042cf6f8e675
Jerod Estapa <jerodestapa@gmail.com>
2015-07-09 16:44:25 -0500

    Task 5 complete

--- contact.js
+++ contact.js
@@ -1,13 +1,19 @@
 var Contact = {}
 
 Contact.parseName = function(str) {
-	var res = str.split(",")
-	return res[0]
-};
+	return str.split(",")[0].trim()
+}
 
 Contact.parseNumber = function(str) {
-	var res = str.split(",")
-	return res[1]
+	return str.split(",")[1].trim()
+}
+
+Contact.createContact = function(str) {
+	var contact = {
+		name: this.parseName(str),
+		number: this.parseNumber(str)
+	}
+	return contact
 }
 
 module.exports = Contact
--- test.js
+++ test.js
@@ -7,6 +7,8 @@ var MyContact = require('./contact')
 
 var name = MyContact.parseName('Jerod Estapa,555-555-5555')
 var number = MyContact.parseNumber('Jerod Estapa,555-555-5555')
+var contact = MyContact.createContact('Jerod Estapa,555-555-5555')
 
 console.log(name)
 console.log(number)
+console.log(contact)
