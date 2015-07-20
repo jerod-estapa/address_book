843c307628295938ff22f33f71356189e388621a
Jerod Estapa <jerodestapa@gmail.com>
2015-07-20 16:45:22 -0500

    Added Command object

--- command.js
+++ command.js
@@ -1,9 +1,11 @@
-var Command = {};
+var Command = {}
 
 Command.getOperation = function() {
-
+	return process.argv[2]
 }
 
 Command.getOperationData = function() {
-	
+	return process.argv[3]
 }
+
+modules.export = Command
